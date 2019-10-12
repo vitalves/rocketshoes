a31e014d16042052376e21d37c99d3506ffafd17
vitalves <vitalvesilva@gmail.com>
2019-10-12 18:07:38 -0300

    Adicionando ao carrinho:  dispatch

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -23,6 +23,15 @@ class Home extends Component {
     this.setState({ products: data });
   }
 
+  handleAddProduct = product => {
+    const { dispatch } = this.props;
+
+    dispatch({
+      type: 'ADD_TO_CART',
+      product,
+    });
+  };
+
   render() {
     const { products } = this.state;
 
@@ -33,7 +42,10 @@ class Home extends Component {
             <img src={product.image} alt={product.title} />
             <strong> {product.title} </strong>
             <span> {product.priceFormatted} </span>
-            <button type="button">
+            <button
+              type="button"
+              onClick={() => this.handleAddProduct(product)}
+            >
               <div>
                 <MdAddShoppingCart size={16} color="#fff" /> 3
               </div>
