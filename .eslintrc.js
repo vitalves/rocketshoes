ccb2d3f95feb96480fbc0a84eb12f34c123e168e
vitalves <vitalvesilva@gmail.com>
2019-10-21 17:43:18 -0300

    React Hooks: config .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,6 +23,7 @@ module.exports = {
   plugins: [
     'react',
     'prettier',
+    'react-hooks',
   ],
   rules: {
     'prettier/prettier': 'error',
@@ -34,6 +35,9 @@ module.exports = {
     ],
     'import/prefer-default-export' : 'off',
     'no-param-reassign': 'off',
-    'no-console': ["error", { allow: ["tron"]}]
+    'no-console': ["error", { allow: ["tron"]}],
+
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
   },
 };
